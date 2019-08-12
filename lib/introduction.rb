206fe4846384ebfb8ed05c792b3958fad9cb7503
James Batsell <jamesbatsell05@gmail.com>
2019-08-12 08:32:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,12 +11,14 @@ def introduction_with_language (name, language)
 end
 
 introduction_with_language("Dan", "Ember.js")
-introduction_with_language("Josh", )
+introduction_with_language("Josh", "React")
   
   
   def introduction_with_language_optional(name, language = "Ruby")
     puts "hi my name is  #{name} and I am learning to program in #{language}"
   end
+  
+  
   
  
 
